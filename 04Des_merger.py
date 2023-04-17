5d005501769392764a7f15a3c85b3b06f72c6f69
SJTU-MI <95070809+SJTU-MI@users.noreply.github.com>
2023-04-17 10:15:36 +0800

    Descriptors merged from different sources

---
+++ 04Des_merger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+
+path="./data/"
+file0="Des_monomer.csv"
+file1="Des_MD.csv"
+file2="Des_Mordred.csv"
+
+df=pd.read_csv(path+file0)
+df1=pd.read_csv(path+file1)
+df2=pd.read_csv(path+file2)
+df11=df1.drop(df1.columns[0:2], axis=1)
+df21=df2.drop(df2.columns[0:3], axis=1)
+df_des=df.join(df11)
+df_des=df_des.join(df21)
+df_des.to_csv(path+"Des_optimized.csv",index=None)
